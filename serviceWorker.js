fd7227350709e00bf59fa6fedcd0229b6922bd65
dmytrkat <126799547+KaterynaDmytryienko@users.noreply.github.com>
2024-05-27 23:27:53 +0200

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,9 +1,18 @@
 const CACHE_NAME = 'v1';
+/**
+ * An array of local resources that we want to cache.
+ * @const {Array<string>} cacheFiles - Files to be cached for offline use.
+ */
 const cacheFiles = [
     'fallback.html',
     'fallback-js.js',
 ];
-
+/**
+ * Handles the 'install' event for the service worker.
+ * Caches necessary files specified in `cacheFiles`.
+ * 
+ * @param {Event} event - The install event.
+ */
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
@@ -12,6 +21,12 @@ self.addEventListener('install', (event) => {
     );
 });
 
+/**
+ * Handles the 'activate' event for the service worker.
+ * Clears old caches that do not match the current cache name.
+ * 
+ * @param {Event} event - The activate event.
+ */
 self.addEventListener('activate', (event) => {
     event.waitUntil(
         caches.keys().then((cacheNames) => {
@@ -26,6 +41,12 @@ self.addEventListener('activate', (event) => {
     );
 });
 
+/**
+ * Handles 'fetch' events by trying to respond with cached files.
+ * If a network request fails, falls back to a cached offline page.
+ * 
+ * @param {Event} event - The fetch event.
+ */
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
